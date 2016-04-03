8f8db726e5ab30fca215b1d7f93bbb3c581158c9
kasheemlew <417879751@qq.com>
2016-04-03 21:07:47 +0800

    simple test

--- culfes/app/main/views.py
+++ culfes/app/main/views.py
@@ -8,3 +8,7 @@ from flask import render_template
 def test():
     return "<h1>just tell you everything is ok!</h1>"
 
+
+@main.route('/')
+def index():
+    return render_template('/main/index.html')
