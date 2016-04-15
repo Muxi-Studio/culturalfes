f2a1d210f1fdfcd1fa6a597d4dae20eebc0b5fd2
kasheemlew <417879751@qq.com>
2016-04-15 18:44:01 +0800

    add upload files

--- culfes/app/main/views.py
+++ culfes/app/main/views.py
@@ -1,14 +1,33 @@
 # coding: utf-8
 from . import main
-from flask import render_template
+from flask import render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+import os
 
+UPLOAD_FOLDER = '/Users/kasheemlew/Downloads/upload'
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'avi']
 
-# test views
-@main.route('/test/')
-def test():
-    return "<h1>just tell you everything is ok!</h1>"
-
+def allowed_file(filename):
+    if '.' in filename and \
+            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS:
+                return True
 
 @main.route('/')
 def index():
     return render_template('/main/index.html')
+
+
+@main.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for('upload_file', filename=filename))
+    return render_template('upload_file')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER,filename)
