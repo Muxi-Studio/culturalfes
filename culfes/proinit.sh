3995c04f867ca6298125f04772cd9ad120b2198b
kasheemlew <417879751@qq.com>
2016-05-15 16:01:32 +0800

    add duoshuo plugin

--- culfes/proinit.sh
+++ culfes/proinit.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+sudo git clone https://github.com/duoshuo/duoshuo-python-sdk.git
+cd duoshuo-python-sdk
+python setup.py install
+cd ..
+
 cd gt-python-sdk
 sudo python setup.py install
 cd ..
+
 sudo pip install -r requirement.txt
 python manage.py db init
 python manage.py db migrate
