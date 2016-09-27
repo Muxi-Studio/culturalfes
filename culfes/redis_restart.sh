397557f722ddbedc7497025a06a04e1d333dc0b8
neo1218 <neo1218@yeah.net>
2016-09-27 12:58:06 +0800

    updated

--- culfes/redis_restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-redis-cli -p 6379 SHUTDOWN
-redis-cli -p 6380 SHUTDOWN
-redis-cli -p 6381 SHUTDOWN
-redis-cli -p 6382 SHUTDOWN
-redis-cli -p 6383 SHUTDOWN
-
-/bin/sh ~/www/culturalfes/culfes/redis.sh
